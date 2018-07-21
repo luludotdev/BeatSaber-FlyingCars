f37900a1b683b1bf3349b216324a0a35cef96c8c
Jack Baron <jackmbaron@gmail.com>
2018-07-21 22:10:06 +0100

    Enable cars

---
+++ FlyingCarEnabler/Plugin.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using IllusionPlugin;
+
+namespace FlyingCarEnabler
+{
+    public class Plugin : IPlugin
+    {
+        public string Name => "Flying Car Enabler";
+        public string Version => "0.1.0";
+
+        public void OnApplicationStart()
+        {
+            Console.WriteLine("[Flying Car Enabler] Loaded.");
+            FlyingCar.startflyingCars = true;
+        }
+
+        public void OnApplicationQuit()
+        {
+        }
+
+        public void OnLevelWasLoaded(int level)
+        {
+        }
+
+        public void OnLevelWasInitialized(int level)
+        {
+        }
+
+        public void OnUpdate()
+        {
+        }
+
+        public void OnFixedUpdate()
+        {
+        }
+    }
+}
