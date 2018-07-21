ea93f0a270fc1961759f8fd02447dd98bef0403a
Jack Baron <jackmbaron@gmail.com>
2018-07-21 22:21:22 +0100

    Update assembly info

--- FlyingCarEnabler/Properties/AssemblyInfo.cs
+++ FlyingCarEnabler/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("FlyingCarEnabler")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("Flying Car Enabler")]
+[assembly: AssemblyDescription("Simple plugin to globally enable the Flying Cars easter egg")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("FlyingCarEnabler")]
-[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyCompany("Jack Baron DEV")]
+[assembly: AssemblyProduct("Flying Car Enabler")]
+[assembly: AssemblyCopyright("Copyright © Jack Baron DEV 2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.1.0.0")]
+[assembly: AssemblyFileVersion("0.1.0.0")]
