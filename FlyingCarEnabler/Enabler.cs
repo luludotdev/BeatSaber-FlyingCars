b1f8d8ffa36f94b398fa2baafd2c972bd94c41f4
Jack Baron <jackmbaron@gmail.com>
2018-07-21 21:51:08 +0100

    Add project files.

---
+++ FlyingCarEnabler/Enabler.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using Object = UnityEngine.Object;
+using IllusionPlugin;
+
+namespace FlyingCarEnabler
+{
+    public class Enabler : MonoBehaviour
+    {
+        public static Enabler Instance = null;
+        float carTime = 0;
+
+        public static void OnLoad()
+        {
+            if (Instance != null) return;
+            new GameObject("Flying Car Enabler").AddComponent<Enabler>();
+        }
+    }
+
+    public void Update()
+    {
+        Debug.Log("Whee");
+    }
+}
